35384c66e8623bd0097ffd3d37fdaa299cc63915
Martin Evans <martinjosephevans@googlemail.com>
2019-04-26 17:25:04 +0100

    Initial solution creation and layout, including a prototype ui attempt

---
+++ Minefield.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Minefield.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(RenderBoard());
+
+            //System.Console.ReadLine();
+
+        }
+
+        private static string RenderBoard(){
+            return @"
+  _ _ _ _ _ _ _ _
+8|_|_|_|_|_|_|_|_|            
+7|_|_|_|_|_|_|_|_|
+6|_|_|_|_|_|_|_|_|
+5|_|_|_|_|_|_|_|_|
+4|_|_|_|_|_|_|_|_|
+3|_|X|_|_|_|_|_|_|            
+2|_|_|_|_|_|_|_|s|
+1|_|_|_|_|_|_|_|s|
+  a b c d e f g h
+";
+        }
+
+    }
+}
