c588d4b0eaab016deb26cd3f2b87c925bf1b6824
Martin Evans <martinjosephevans@googlemail.com>
2021-07-13 08:19:14 +0100

    Removed unnecessary obj folders

--- Minefield.Core.Tests.Unit/TheGameBoard.cs
+++ Minefield.Core.Tests.Unit/TheGameBoard.cs
@@ -1,32 +1,40 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Minefield.Core.Tests.Unit
 {
     [TestClass]
-    public class TheGameBoard{
-
+    public class TheGameBoard
+    {
         [TestMethod]
-        public void ConsistsOf_An8x8Grid(){
-
+        public void ConsistsOf_An8x8Grid()
+        {
             var board = new Board();
             Assert.AreEqual(8, board.Rows);
             Assert.AreEqual(8, board.Columns);
-
         }
 
-         [TestMethod]
-        public void ConsistsOf_10RandomlyPlacedMines(){
-
+        [TestMethod]
+        public void ConsistsOf_10RandomlyPlacedMines()
+        {
             var board = new Board();
-            
+
             Assert.AreEqual(10, board.Mines.Length);
 
-            foreach(var location in board.Mines){
-                System.Console.WriteLine($"{location.Column}{location.Row}");
+            foreach (var location in board.Mines)
+            {
+                Console.WriteLine($"{location.Column}{location.Row}");
             }
-
         }
-
     }
     
+    [TestClass]
+    public class BreakingTest
+    {
+        [TestMethod]
+        public void Foo()
+        {
+            Assert.AreEqual(1 ==2, "This test was expected to fail");
+        }
+    }
 }
